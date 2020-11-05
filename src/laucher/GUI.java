5f37782f616b3b0d034a67d94d797cdef60904d7
jakubpartyka <j.partyka20@gmail.com>
2020-11-05 16:10:20 +0100

    basic add conf form implemented

--- src/laucher/GUI.java
+++ src/laucher/GUI.java
@@ -1,15 +1,19 @@
 package laucher;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI implements Runnable {
     JPanel mainPanel;
     JTabbedPane tabbedPane;
+    private JTextField textField1;
+    private JButton addConfigurationButton;
     JFrame frame = new JFrame();
 
     private void initMainFrame() {
         // set frame properties
         frame.setSize(800,600);
+        frame.setMinimumSize(new Dimension(300,300));
         frame.setResizable(true);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
