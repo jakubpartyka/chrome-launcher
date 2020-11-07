45fcc2112b3abbf93cdf73691e73de5d94a69097
jakubpartyka <j.partyka20@gmail.com>
2020-11-07 20:27:42 +0100

    ConfigurationsTable class added

---
+++ src/laucher/data/ConfigurationsTable.java
@@ -0,0 +1,20 @@
+package laucher.data;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ConfigurationsTable extends AbstractTableModel {
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+}
