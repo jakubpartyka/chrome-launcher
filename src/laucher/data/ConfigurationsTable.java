c62ddc58f9f115fe24f31d7b09796e5e3acb835b
jakubpartyka <j.partyka20@gmail.com>
2020-11-07 20:30:14 +0100

    ConfigurationsTable basic implementation

--- src/laucher/data/ConfigurationsTable.java
+++ src/laucher/data/ConfigurationsTable.java
@@ -5,16 +5,24 @@ import javax.swing.table.AbstractTableModel;
 public class ConfigurationsTable extends AbstractTableModel {
     @Override
     public int getRowCount() {
-        return 0;
+        return Configuration.configurationList.size();
     }
 
     @Override
     public int getColumnCount() {
-        return 0;
+        return 5;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return null;
+        Configuration conf = Configuration.configurationList.get(rowIndex);
+        return switch (columnIndex) {
+            case 1 -> conf.alias;
+            case 2 -> conf.proxyAddress;
+            case 3 -> conf.proxyPort;
+            case 4 -> conf.proxyUser;
+            case 5 -> conf.proxyPass;
+            default -> "N/A";
+        };
     }
 }
