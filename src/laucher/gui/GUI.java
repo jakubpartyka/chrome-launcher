0e866b66dab40d8f9e5b8d0e59435f52ca14d6e2
jakubpartyka <j.partyka20@gmail.com>
2020-11-10 11:36:15 +0100

    dataTable displaying improved ; column resizing to content implemented

--- src/laucher/gui/GUI.java
+++ src/laucher/gui/GUI.java
@@ -5,6 +5,8 @@ import laucher.data.ConfigurationsTableModel;
 import laucher.data.DataReaderWriter;
 
 import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.io.IOException;
 
@@ -37,6 +39,8 @@ public class GUI implements Runnable {
 
         // init table model
         dataTable.setModel(new ConfigurationsTableModel());
+        dataTable.setForeground(Color.WHITE);
+        resizeColumnWidth(dataTable);
 
         // adding components
         frame.add(mainPanel);
@@ -106,6 +110,21 @@ public class GUI implements Runnable {
             }
         });
 
+    }
+
+    public void resizeColumnWidth(JTable table) {
+        final TableColumnModel columnModel = table.getColumnModel();
+        for (int column = 0; column < table.getColumnCount()-1; column++) {
+            int width = 15; // Min width
+            for (int row = 0; row < table.getRowCount(); row++) {
+                TableCellRenderer renderer = table.getCellRenderer(row, column);
+                Component comp = table.prepareRenderer(renderer, row, column);
+                width = Math.max(comp.getPreferredSize().width +1 , width);
+            }
+            if(width > 300)
+                width=300;
+            columnModel.getColumn(column).setPreferredWidth(width);
+        }
     }
 
 }
