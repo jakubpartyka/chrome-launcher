140604095e76e061a63054104f2fad807094e13d
jakubpartyka <j.partyka20@gmail.com>
2020-12-18 13:38:33 +0100

    Exception handling changed

--- src/laucher/data/Encryptor.java
+++ src/laucher/data/Encryptor.java
@@ -7,26 +7,27 @@ import java.security.NoSuchAlgorithmException;
 public class Encryptor {
 
     public static boolean hashMatch(String input, String originalHash){
-        try {
-            String hashed = encryptSHA256(input);
-            return hashed.equals(originalHash);
-        } catch (NoSuchAlgorithmException e){
+        String hashed = encryptSHA256(input);
+        if(hashed == null)
             return false;
-        }
+        return hashed.equals(originalHash);
     }
 
-    private static String encryptSHA256(String originalString) throws NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(originalString.getBytes(StandardCharsets.UTF_8));
+    public static String encryptSHA256(String originalString) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(originalString.getBytes(StandardCharsets.UTF_8));
 
-        StringBuilder hexString = new StringBuilder(2 * hash.length);
-        for (byte b : hash) {
-            String hex = Integer.toHexString(0xff & b);
-            if (hex.length() == 1) {
-                hexString.append('0');
+            StringBuilder hexString = new StringBuilder(2 * hash.length);
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
             }
-            hexString.append(hex);
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e){
+            return null;
         }
-        return hexString.toString();
     }
 }
